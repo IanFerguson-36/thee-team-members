355d07d6f695c7566795694123f7a99dc5adbc4c
Ian F <ian.ferg36@gmail.com>
2021-04-13 19:43:30 -0600

    created getmanager function and set up functions for engineers and interns

--- index.js
+++ index.js
@@ -8,7 +8,13 @@
 // Giving the name of pageTemplate
 const inquirer = require("inquirer");
 const manager = require("./lib/Manager");
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
 const pageTemplate = require("./src/page-template.js");
+const path = require("path");
+const fs = require("fs");
+const membersArray = [];
 // And now, we can use that pageTemplate as a function, which can ACCEPT a parameter
 // pageTemplate(answers_from_inquirer_prompt);
 // INDEX FILES ARE CONSIDERED THE ENTRY POINT TO YOUR APPLICATION
@@ -18,37 +24,44 @@ const pageTemplate = require("./src/page-template.js");
 // THIS IS WHERE YOU DO YOUR FS WRITEFILE STUFF
 
 // THE DIST FOLDER IS WHERE THE OUTPUT HTML FILES WILL LAND
+
 function runApp() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "managerName",
-        message: "What is the team manager's name?",
-      },
-      {
-        type: "input",
-        name: "managerId",
-        message: "What is the team manager's id?",
-      },
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the team manager's email?",
-      },
-      {
-        type: "input",
-        name: "managerOfficeNum",
-        message: "What is the team manager's office number?",
-      },
-    ])
-    .then((answers) => {
-      console.log(answers);
-      const manager = new Manager(
-        answers.managerName,
-        answers.managerEmail,
-        answers.managerOfficeNum
-      );
-    });
+  function createManager() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the team manager's name?",
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the team manager's id?",
+        },
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the team manager's email?",
+        },
+        {
+          type: "input",
+          name: "managerOfficeNum",
+          message: "What is the team manager's office number?",
+        },
+      ])
+      .then((answers) => {
+        console.log(answers);
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerEmail,
+          answers.managerOfficeNum
+        );
+        console.log(manager);
+        membersArray.push(manager);
+      });
+  }
+  function createEngineer() {}
+  function createIntern() {}
 }
 runApp();
